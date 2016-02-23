add13c275fe83ef0ef66638afe122a01d50ff192
Meredith Underell <meredith.underell@socrata.com>
2016-02-23 10:55:59 -0800

    Added Douglas County logos and removed unused comments

--- app/assets/js/carousel.js
+++ app/assets/js/carousel.js
@@ -7,10 +7,8 @@ $(document).ready(function() {
     url: domain + '/resource/' + carouselDatasetId + '.json?'
   }).done(function (data) {
     for (var i = 0; i < data.length; i++) {
-      /*document.getElementById('carousel-content').innerHTML += '<div class="slick-slide" data-slick-index="' + [i] + '"><div class="image"><img src="' + domain + '/views/' + carouselDatasetId + '/files/' + data[i].image + '" /></div></div>';*/
       document.getElementById('carousel-content').innerHTML += '<div id="slide' + [i] + '" class="slick-slide" data-slick-index="' + [i] + '"></div>';
       $('#slide' + [i]).css('background-image', 'url(' + domain + '/views/' + carouselDatasetId + '/files/' + data[i].image + ')');
-      /*document.getElementById('carousel-content').innerHTML += '<img src="' + domain + '/views/' + carouselDatasetId + '/files/' + data[i].image + '" />';*/
     }   
 
     $('.slick-track').slick({
